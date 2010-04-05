211c699d5e7e03bdce2d23cfc4ab09bbc568c17a
Aşkın Yollu <askin@askin.ws>
2010-04-05 04:30:12 +0300

    for module

---
+++ GNazar/__init__.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under GPL v2
+# Copyright 2010, Aşkın Yollu <askin@askin.ws>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+__version__ = "0.1"
+__all__ = ["GNazar"]
