15f2c866d741428cfe9168452f4496b3ad69faa5
Aşkın Yollu <askin@askin.ws>
2013-02-20 16:18:52 +0200

    pep8ify

--- GNazar/gnazar.py
+++ GNazar/gnazar.py
@@ -17,7 +17,9 @@ import gtk
 import gettext
 import pynotify
 import time
-import os, sys, locale
+import os
+import sys
+import locale
 import random
 import platform
 gtk.gdk.threads_init()
@@ -30,12 +32,15 @@ sharedirs = '/usr/share'
 
 _ = gettext.gettext
 
+
 class GNazar():
     def __init__(self):
         # create a new Status Icon
         self.gnazar = gtk.StatusIcon()
-        self.gnazar.set_from_file('%s/icons/hicolor/22x22/apps/gnazar-deactive.png' % sharedirs)
-        self.gnazar.set_tooltip(_("GNazar - You are completely demilitarized..."))
+        self.gnazar.set_from_file(
+            '%s/icons/hicolor/22x22/apps/gnazar-deactive.png' % sharedirs)
+        self.gnazar.set_tooltip(
+            _("GNazar - You are completely demilitarized..."))
         self.gnazar.set_visible(True)
         self.status = False
 
@@ -82,7 +87,8 @@ class GNazar():
     show popup menu
     '''
     def show_menu(self, status_icon, button, activate_time):
-        self.menu.popup(None, None, gtk.status_icon_position_menu, button, activate_time, status_icon)
+        self.menu.popup(None, None, gtk.status_icon_position_menu,
+                        button, activate_time, status_icon)
 
     # random notification
     def _notification(self):
@@ -97,14 +103,16 @@ class GNazar():
     def show_about(self, widget):
         about = gtk.AboutDialog()
         about.set_program_name("GNazar")
-        about.set_icon_from_file("%s/icons/hicolor/22x22/apps/gnazar.png" % sharedirs)
+        about.set_icon_from_file("%s/icons/hicolor/22x22/apps/gnazar.png"
+                                 % sharedirs)
         about.set_version("0.1")
         about.set_copyright("(c) Aşkın Yollu")
         # FIXME: make it generic (mac, bsd, win etc..)
         dist_name = platform.dist()[0]
         about.set_comments(_("GNazar is a useful part of the %s" % dist_name))
         about.set_website("http://www.askin.ws")
-        about.set_logo(gtk.gdk.pixbuf_new_from_file("%s/icons/hicolor/32x32/apps/gnazar.png" % sharedirs))
+        about.set_logo(gtk.gdk.pixbuf_new_from_file(
+                "%s/icons/hicolor/32x32/apps/gnazar.png" % sharedirs))
         about.set_translator_credits(_("TRANSLATORS"))
         about.set_artists([_("THANKSFORICONS")])
         about.run()
@@ -120,34 +128,39 @@ class GNazar():
     def protect(self, widget):
         if self.status == False:
             dialog = gtk.MessageDialog(
-                parent         = None,
-                flags          = gtk.DIALOG_DESTROY_WITH_PARENT,
-                type           = gtk.MESSAGE_INFO,
-                buttons        = gtk.BUTTONS_OK,
-                message_format = _("GNazar is starting to protect your computer from harmful looks...")
+                parent=None,
+                flags=gtk.DIALOG_DESTROY_WITH_PARENT,
+                type=gtk.MESSAGE_INFO,
+                buttons=gtk.BUTTONS_OK,
+                message_format=_("GNazar is starting to protect your "
+                                 "computer from harmful looks...")
                 )
             dialog.set_title(_("GNazar Application"))
             dialog.connect('response', self.dialog_destroyer)
             dialog.show()
             self.status = True
             self.gnazar.set_tooltip(_("GNazar - No harmful look allowed!"))
-            self.gnazar.set_from_file("%s/icons/hicolor/22x22/apps/gnazar.png" % sharedirs)
+            self.gnazar.set_from_file("%s/icons/hicolor/22x22/apps/gnazar.png"
+                                      % sharedirs)
 
     def release(self, widget):
         if self.status == True:
             dialog = gtk.MessageDialog(
-                parent         = None,
-                flags          = gtk.DIALOG_DESTROY_WITH_PARENT,
-                type           = gtk.MESSAGE_WARNING,
-                buttons        = gtk.BUTTONS_OK,
-                message_format = _("GNazar is stopping to protect your computer from harmful looks...")
+                parent=None,
+                flags=gtk.DIALOG_DESTROY_WITH_PARENT,
+                type=gtk.MESSAGE_WARNING,
+                buttons=gtk.BUTTONS_OK,
+                message_format=_("GNazar is stopping to protect your computer"
+                                 " from harmful looks...")
                 )
             dialog.set_title(_("GNazar Application"))
             dialog.connect('response', self.dialog_destroyer)
             dialog.show()
             self.status = False
-            self.gnazar.set_tooltip(_("GNazar - You are completely demilitarized..."))
-            self.gnazar.set_from_file("%s/icons/hicolor/22x22/apps/gnazar-deactive.png" % sharedirs)
+            self.gnazar.set_tooltip(
+                _("GNazar - You are completely demilitarized..."))
+            self.gnazar.set_from_file(
+                "%s/icons/hicolor/22x22/apps/gnazar-deactive.png" % sharedirs)
 
     def notification(self):
         self.total_attack += 1
@@ -160,8 +173,13 @@ class GNazar():
             title = _("Nazar harmed")
             body = _("Nazar Received and it HARMED!")
             icon = "dialog-warning"
-            self.gnazar.set_tooltip(_("GNazar - %s attacks received so far, %s are defated and %s are received...") %
-                                    (self.total_attack, self.defated_attack, self.total_attack - self.defated_attack))
+            self.gnazar.set_tooltip(
+                _("GNazar - %s attacks received so far, %s"
+                  " are defated and %s are received...") %
+                (self.total_attack,
+                 self.defated_attack,
+                 self.total_attack - self.defated_attack))
+
         notify = pynotify.Notification(title, body, icon)
         notify.set_urgency(pynotify.URGENCY_NORMAL)
         notify.set_timeout(pynotify.EXPIRES_NEVER)
@@ -169,6 +187,7 @@ class GNazar():
 
     def dialog_destroyer(self, dialog, widget):
         dialog.destroy()
+
 
 def main():
     si = GNazar()
