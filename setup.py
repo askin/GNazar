6773d08bf091eedba13e0ba92235cda18068d8a9
Aşkın Yollu <askin@askin.ws>
2013-02-21 16:26:32 +0200

    add comment line to setup update version

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ import msgfmt
 import os
 
 
+# Build translation files
 class build_trans(cmd.Command):
     description = 'Compile .po files into .mo files'
     user_options = []
@@ -55,6 +56,7 @@ class build_trans(cmd.Command):
                             msgfmt.make(src, dest)
 
 
+# Override build class to compile translation files
 class build(_build):
     sub_commands = _build.sub_commands + [('build_trans', None)]
 
@@ -62,6 +64,7 @@ class build(_build):
         _build.run(self)
 
 
+# Override install_data class to install translation files
 class install_data(_install_data):
 
     def run(self):
@@ -73,6 +76,7 @@ class install_data(_install_data):
         _install_data.run(self)
 
 
+# installation steps
 cmdclass = {
     'build': build,
     'build_trans': build_trans,
@@ -80,6 +84,7 @@ cmdclass = {
 }
 
 
+# data files
 datas = [('', ['README', 'COPYING', 'AUTHORS', 'ChangeLog']),
          ('../applications', ['data/gnazar.desktop']),
          ('../icons/hicolor/32x32/apps',
@@ -96,8 +101,9 @@ datas = [('', ['README', 'COPYING', 'AUTHORS', 'ChangeLog']),
           ['build/locale/en/LC_MESSAGES/gnazar.mo'])]
 
 
+# Call setup
 setup(name='GNazar',
-      version='0.1',
+      version='0.2',
       description='GTK Nazar Aplication',
       author='Aşkın Yollu',
       author_email='askin@askin.ws',
